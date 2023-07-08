222ad8df69d313fd1d7807d7c10597eced32f074
Woze Parrot <wozeparrot@gmail.com>
2023-07-08 15:56:36 -0400

    feat: better member logging

--- tinymod/plugins/logging.py
+++ tinymod/plugins/logging.py
@@ -13,7 +13,7 @@ DATABASE = "tinymod.db"
 async def setup(_):
   async with aiosqlite.connect(DATABASE) as db:
     await db.execute("CREATE TABLE IF NOT EXISTS logging_messages (id TEXT PRIMARY KEY, timestamp INTEGER, channel_id INTEGER, user_id INTEGER, username TEXT, action TEXT, content TEXT, json TEXT)")
-    await db.execute("CREATE TABLE IF NOT EXISTS logging_members (id TEXT PRIMARY KEY, timestamp INTEGER, user_id INTEGER, username TEXT, json TEXT)")
+    await db.execute("CREATE TABLE IF NOT EXISTS logging_members (id TEXT PRIMARY KEY, timestamp INTEGER, user_id INTEGER, username TEXT, action TEXT, json TEXT)")
     await db.commit()
 
 # message logging
@@ -33,10 +33,18 @@ async def message_update(client: Client, message: Message, _): await log_message
 async def message_delete(client: Client, message: Message): await log_message("delete", message)
 
 # member logging
-@TinyMod.events
-async def guild_user_edit(client: Client, guild: Guild, user: ClientUserBase, _):
-  profile: GuildProfile | None = user.get_guild_profile_for(guild)
+async def log_member(action: str, user: ClientUserBase):
+  profile: GuildProfile | None = user.get_guild_profile_for(GUILD)
   if profile is None: return
   async with aiosqlite.connect(DATABASE) as db:
-    await db.execute("INSERT INTO logging_members (id, timestamp, user_id, username, json) VALUES (?, ?, ?, ?, ?)", (str(uuid.uuid4()), time.monotonic(), user.id, user.full_name, json.dumps(profile.to_data(include_internals=True))))
+    await db.execute("INSERT INTO logging_members (id, timestamp, user_id, username, action, json) VALUES (?, ?, ?, ?, ?, ?)", (str(uuid.uuid4()), time.monotonic(), user.id, user.full_name, action, json.dumps(profile.to_data(include_internals=True))))
     await db.commit()
+
+@TinyMod.events
+async def guild_user_add(client: Client, guild: Guild, user: ClientUserBase): await log_member("add", user)
+
+@TinyMod.events
+async def guild_user_update(client: Client, guild: Guild, user: ClientUserBase, _): await log_member("update", user)
+
+@TinyMod.events
+async def guild_user_delete(client: Client, guild: Guild, user: ClientUserBase, _): await log_member("delete", user)
