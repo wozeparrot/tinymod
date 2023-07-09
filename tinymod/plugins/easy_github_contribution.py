1b14a4d614562555965d8d2e283888480ddc647f
Woze Parrot <wozeparrot@gmail.com>
2023-07-09 17:24:52 -0400

    feat: support commit links as well

--- tinymod/plugins/easy_github_contribution.py
+++ tinymod/plugins/easy_github_contribution.py
@@ -2,7 +2,7 @@ from hata import Client, Guild, InteractionEvent, Message, Role
 from hata.ext.slash import Button, Row, ButtonStyle
 from github import Github, Auth
 
-import os
+import os, re
 
 TinyMod: Client
 GUILD: Guild
@@ -20,21 +20,44 @@ COMPONENTS = Row(ACCEPT_BUTTON, DENY_BUTTON)
 async def message_create(client: Client, message: Message):
   # check channel id
   if message.channel.id != CHANNEL_ID: return
-  # check if message is from bot
+  # check if message is from a bot
   if message.author.bot: return
 
   # quick returns for messages that don't need to be manually checked
   # check if the user already has the role
-  if message.author.has_role(ROLE):
-    return await client.message_delete(message)
-  # check if the message is a link to a pr
+  if message.author.has_role(ROLE): return await client.message_delete(message)
+  if message.content is None: return await client.message_delete(message)
+  # check if the message is a link to something in the tinygrad repo
   if not ("https://github.com/tinygrad/tinygrad/" in message.content or "http://github.com/tinygrad/tinygrad/" in message.content):
     return await client.message_delete(message)
-  # check if the pr is merged
-  if not GITHUB.get_repo("tinygrad/tinygrad").get_pull(int(message.content.split("/")[-1])).merged:
+
+  dm_channel = await client.channel_private_create(message.author)
+
+  # check if the link is to a pr
+  if "/pull/" in message.content:
+    # check if the pr is merged
+    if not GITHUB.get_repo("tinygrad/tinygrad").get_pull(int(message.content.split("/")[-1])).merged:
+      # send a direct message to the user
+      await client.message_create(dm_channel, "Your PR is not merged yet. Please wait for it to be merged before posting it.")
+      return await client.message_delete(message)
+  # check if the link is a merge commit
+  elif "/commit/" in message.content:
+    # try to pull out the pr number from the commit message
+    commit_message = GITHUB.get_repo("tinygrad/tinygrad").get_commit(message.content.split("/")[-1]).commit.message
+    pr_number = re.search(r"#(\d+)", commit_message)
+    # check if the pr number was found
+    if pr_number is None:
+      # send a direct message to the user
+      await client.message_create(dm_channel, "Please post the link to the PR instead.")
+      return await client.message_delete(message)
+    # check if the pr is merged
+    if not GITHUB.get_repo("tinygrad/tinygrad").get_pull(int(pr_number.group(1))).merged:
+      # send a direct message to the user
+      await client.message_create(dm_channel, "Your PR is not merged yet. Please wait for it to be merged before posting it.")
+      return await client.message_delete(message)
+  else:
     # send a direct message to the user
-    dm_channel = await client.channel_private_create(message.author)
-    await client.message_create(dm_channel, "Your PR is not merged yet. Please wait for it to be merged before posting it.")
+    await client.message_create(dm_channel, "Please post the link to the PR instead.")
     return await client.message_delete(message)
 
   # reply with the components for admins to accept or deny
