64503a0fba3b4eb6fea0e09744e1df8259190fe9
Woze Parrot <wozeparrot@gmail.com>
2023-07-08 20:44:31 -0400

    feat: extended modmenu

---
+++ tinymod/plugins/moderation_menu.py
@@ -0,0 +1,13 @@
+from hata import Client, Guild, Permission, Role
+
+TinyMod: Client
+GUILD: Guild
+ADMIN_ROLE: Role
+
+@TinyMod.interactions(guild=GUILD, show_for_invoking_user_only=True, target="user")
+async def softban(client, event, user):
+  """Bans a user, then unbans them, deleting their messages in the process."""
+  if not event.user.has_role(ADMIN_ROLE): return
+  await client.guild_ban_add(event.guild, user, delete_message_duration=604800, reason="Softban")
+  await client.guild_ban_delete(event.guild, user, reason="Softban")
+
