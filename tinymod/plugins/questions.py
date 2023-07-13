eff503740178d9db5e9eb3de8b2f75b84010d403
John Boeggeman <boeggeman97@gmail.com>
2023-07-13 18:12:19 -0500

    Added command to link a user to smart questions

---
+++ tinymod/plugins/questions.py
@@ -0,0 +1,10 @@
+from hata import Client, Guild, Message, User
+
+TinyMod: Client
+GUILD: Guild
+
+@TinyMod.interactions(guild=GUILD)
+async def question(_, user: User):
+  """Recommends reading How To Ask Questions The Smart Way to a user."""
+  # TODO: explude roles?
+  return Message(f'Hello {user.mention}, I would recommend reading: [How To Ask Questions The Smart Way](http://www.catb.org/~esr/faqs/smart-questions.html).')
