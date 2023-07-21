ebc41ff1de8f3dabfb0b856bcd9d27a091893bbf
Woze Parrot <wozeparrot@gmail.com>
2023-07-21 14:32:27 -0400

    feat: should really be called questions

--- tinymod/plugins/questions.py
+++ tinymod/plugins/questions.py
@@ -4,6 +4,6 @@ TinyMod: Client
 GUILD: Guild
 
 @TinyMod.interactions(guild=GUILD)
-async def question(user: User):
+async def questions(user: User):
   """Recommends reading How To Ask Questions The Smart Way to a user."""
   return f"Hello {user:m}, I would recommend reading: [How To Ask Questions The Smart Way](http://www.catb.org/~esr/faqs/smart-questions.html)."
