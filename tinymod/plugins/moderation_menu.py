da581f52488e922166640b108df1b7fb268f36d9
Woze Parrot <wozeparrot@gmail.com>
2023-08-03 15:48:27 -0400

    fix: fix naming of softkick

--- tinymod/plugins/moderation_menu.py
+++ tinymod/plugins/moderation_menu.py
@@ -8,6 +8,7 @@ ADMIN_ROLE: Role
 async def softkick(client, event, user):
   """Bans a user, then unbans them, deleting their messages in the process."""
   if not event.user.has_role(ADMIN_ROLE): return
-  await client.guild_ban_add(event.guild, user, delete_message_duration=604800, reason="Softban")
-  await client.guild_ban_delete(event.guild, user, reason="Softban")
+  await client.guild_ban_add(event.guild, user, delete_message_duration=604800, reason="Softkick")
+  await client.guild_ban_delete(event.guild, user, reason="Softkick")
+  return f"{user:f} has been softkicked."
 
