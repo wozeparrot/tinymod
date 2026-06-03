d229a5897a1a839e4672ed50b0d14d4f2029804c
Woze Parrot <wozeparrot@gmail.com>
2026-06-03 17:00:45 -0400

    feat: switch to uv + python 3.13

--- flake.nix
+++ flake.nix
@@ -1,61 +1,66 @@
 {
   description = "tinymod: tinygrad's premier discord moderation bot";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    poetry2nix,
+    pyproject-nix,
+    uv2nix,
+    pyproject-build-systems,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication mkPoetryEnv defaultPoetryOverrides;
-    in {
-      packages = {
-        myapp = mkPoetryApplication {projectDir = self;};
-        default = self.packages.${system}.myapp;
-      };
+      python = pkgs.python313;
 
-      devShells.default = (mkPoetryEnv {
-        projectDir = ./.;
-        extraPackages = p: with p; [
-          setuptools
-        ];
-        overrides = defaultPoetryOverrides.extend (self: super: {
-          scarletio = super.scarletio.overridePythonAttrs (old: {
-            propagatedBuildInputs = (old.propagatedBuildInputs or [ ])
-              ++ [ super.setuptools ];
-          });
-          pygal = super.pygal.overridePythonAttrs (old: {
-            propagatedBuildInputs = (old.propagatedBuildInputs or [ ])
-              ++ [ super.pytest-runner ];
-          });
-          hata = super.hata.overridePythonAttrs {
-            # remove the `hata.discord.bin` subpackage
-            postPatch = ''
-              substituteInPlace setup.py --replace "'hata.discord.bin'," ""
-            '';
-          };
-          pygithub = super.pygithub.overridePythonAttrs (old: {
-            propagatedBuildInputs = (old.propagatedBuildInputs or [ ])
-              ++ [ super.setuptools-scm ];
-          });
-        });
-      }).env.overrideAttrs (old: {
-        buildInputs = with pkgs; [
-          poetry
+      workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+      overlay = workspace.mkPyprojectOverlay {sourcePreference = "wheel";};
+
+      pythonSet =
+        (pkgs.callPackage pyproject-nix.build.packages {inherit python;})
+        .overrideScope (nixpkgs.lib.composeManyExtensions [
+          pyproject-build-systems.overlays.default
+          overlay
+        ]);
+
+      venv = pythonSet.mkVirtualEnv "tinymod-env" workspace.deps.default;
+    in {
+      packages.default = venv;
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          venv
+          uv
           sqlite
           sqlite-web
           net-snmp
           ipmitool
         ];
-      });
+        env = {
+          UV_NO_SYNC = "1";
+          UV_PYTHON = "${venv}/bin/python";
+          UV_PYTHON_DOWNLOADS = "never";
+        };
+      };
     });
 }
